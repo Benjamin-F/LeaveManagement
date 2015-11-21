960db6bce3ea811e89bbe22367662d0c7d26f180
heygawau <emilien.garreau@gmail.com>
2015-11-21 12:07:35 +0100

    Add hash method to handle the password

--- src/employes/Employes.java
+++ src/employes/Employes.java
@@ -1,19 +1,27 @@
 package employes;
 
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
 import java.util.Map;
+
+import org.jboss.logging.Logger;
+
+
 
 public class Employes {
 	
 	private static Employes instance = null;
-	private Map <String, String> employe;
+	private Map <String, byte[]> employe;
+	
 	
 	/**
 	 * Constructeur
 	 */
 	private Employes(){
 		super();
-		employe = new HashMap<String, String>();
+		employe = new HashMap<String, byte[]>();
 	}
 	
 	/**
@@ -31,7 +39,15 @@ public class Employes {
 	 * @param pwd
 	 */
 	public void addEmploye(String userName, String pwd){
-		employe.put(userName, pwd);
+		try {
+			employe.put(userName, this.hashPassword(pwd));
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	/**
@@ -41,8 +57,16 @@ public class Employes {
 	 * @return boolean
 	 */
 	public Boolean checkCredentials(String userName, String pwd){
-		String pass = employe.get(userName);
-		
+		byte[] pass = employe.get(userName);
+		try {
+			byte[] hashpwd=this.hashPassword(pwd);
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		if(pass == null)
 			return false;
 		else{
@@ -52,4 +76,24 @@ public class Employes {
 				return false;
 		}
 	}
+	/**
+	 * 
+	 * @param password
+	 * @return Le mot de passe sous forme de hash
+	 * @throws NoSuchAlgorithmException
+	 * @throws UnsupportedEncodingException
+	 */
+	 public byte[] hashPassword(String password) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+	       MessageDigest digest = MessageDigest.getInstance("SHA-256");
+	       digest.reset();
+	       //On update le hash en utilisant une chaine de charactère spécifique
+	       digest.update("bytedigestbfeg".getBytes());
+	       byte[] input = digest.digest(password.getBytes("UTF-8"));
+	       //On boucle pour ralentir les hackers
+	       for (int i = 0; i < 200; i++) {
+	           digest.reset();
+	           input = digest.digest(input);
+	       }
+	       return input;
+	   }
 }
