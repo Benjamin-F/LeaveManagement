d432a9ae2efb97e76438257a48c0ab9430cf86c4
heygawau <emilien.garreau@gmail.com>
2015-11-24 20:55:50 +0100

    modification des bugs du hash et .gitignore

--- src/employes/Employes.java
+++ src/employes/Employes.java
@@ -3,41 +3,41 @@ package employes;
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.jboss.logging.Logger;
 
 
 
 public class Employes {
-	
+
 	private static Employes instance = null;
 	private Map <String, byte[]> employe;
-	
-	
+
+
 	/**
-	 * Constructeur
-	 */
+	* Constructeur
+	*/
 	private Employes(){
 		super();
 		employe = new HashMap<String, byte[]>();
 	}
-	
+
 	/**
 	* @return une instance de Employe
 	*/
 	public static Employes instance(){
 		if(instance==null)
-			instance = new Employes();
+		instance = new Employes();
 		return instance;
 	}
-	
+
 	/**
-	 * Ajoute un employé 
-	 * @param userName
-	 * @param pwd
-	 */
+	* Ajoute un employé
+	* @param userName
+	* @param pwd
+	*/
 	public void addEmploye(String userName, String pwd){
 		try {
 			employe.put(userName, this.hashPassword(pwd));
@@ -49,17 +49,18 @@ public class Employes {
 			e.printStackTrace();
 		}
 	}
-	
+
 	/**
-	 * Vérification userName et pwd
-	 * @param userName
-	 * @param pwd
-	 * @return boolean
-	 */
+	* Vérification userName et pwd
+	* @param userName
+	* @param pwd
+	* @return boolean
+	*/
 	public Boolean checkCredentials(String userName, String pwd){
 		byte[] pass = employe.get(userName);
+		byte[] hashpwd=null;
 		try {
-			byte[] hashpwd=this.hashPassword(pwd);
+			hashpwd=this.hashPassword(pwd);
 		} catch (NoSuchAlgorithmException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -67,33 +68,31 @@ public class Employes {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		if(pass == null)
-			return false;
-		else{
-			if(pass.equals(pwd))
-				return true;
-			else
-				return false;
-		}
+
+		if(Arrays.equals(pass,hashpwd))
+		return true;
+		else
+		return false;
+
 	}
 	/**
-	 * 
-	 * @param password
-	 * @return Le mot de passe sous forme de hash
-	 * @throws NoSuchAlgorithmException
-	 * @throws UnsupportedEncodingException
-	 */
-	 public byte[] hashPassword(String password) throws NoSuchAlgorithmException, UnsupportedEncodingException {
-	       MessageDigest digest = MessageDigest.getInstance("SHA-256");
-	       digest.reset();
-	       //On update le hash en utilisant une chaine de charactère spécifique
-	       digest.update("bytedigestbfeg".getBytes());
-	       byte[] input = digest.digest(password.getBytes("UTF-8"));
-	       //On boucle pour ralentir les hackers
-	       for (int i = 0; i < 200; i++) {
-	           digest.reset();
-	           input = digest.digest(input);
-	       }
-	       return input;
-	   }
+	*
+	* @param password
+	* @return Le mot de passe sous forme de hash
+	* @throws NoSuchAlgorithmException
+	* @throws UnsupportedEncodingException
+	*/
+	public byte[] hashPassword(String password) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+		MessageDigest digest = MessageDigest.getInstance("SHA-256");
+		digest.reset();
+		//On update le hash en utilisant une chaine de charactère spécifique
+		digest.update("bytedigestbfeg".getBytes());
+		byte[] input = digest.digest(password.getBytes("UTF-8"));
+		//On boucle pour ralentir les hackers
+		for (int i = 0; i < 200; i++) {
+			digest.reset();
+			input = digest.digest(input);
+		}
+		return input;
+	}
 }
